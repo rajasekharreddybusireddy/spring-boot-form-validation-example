70527432e8b4940d36f25c3ae6f5e1e288565d16
talk2rajasekhar <talk2rajasekhar>
2019-02-14 11:12:03 +0530

    initial commit

---
+++ src/main/java/com/blogspot/javabyrajasekhar/SpringBootFormValidationExampleApplication.java
@@ -0,0 +1,14 @@
+package com.blogspot.javabyrajasekhar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFormValidationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFormValidationExampleApplication.class, args);
+	}
+
+}
+
